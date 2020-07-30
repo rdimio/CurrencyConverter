81e9ba522742bc35c0715c533da7f14c84ba9860
Vadim Rybakov <vadimio9217@gmail.com>
2020-07-30 15:44:00 +0300

    add database

---
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -0,0 +1,76 @@
+/*
+Создаем базу данных test
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+
+Подключаемся к test, создаем схему currency
+\c test;
+DROP SCHEMA IF EXISTS currency;
+CREATE SCHEMA currency;
+
+Создаем пользователя и настраиваем ему права доступа к базе данных test
+CREATE ROLE financier WITH PASSWORD 'financier' LOGIN;
+GRANT ALL ON SCHEMA currency TO financier;*/
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+    id       SMALLSERIAL,
+    password VARCHAR(255),
+    email    VARCHAR(255) UNIQUE,
+    login    VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles
+(
+    id   SMALLSERIAL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS users_roles;
+CREATE TABLE users_roles
+(
+    user_id SMALLINT NOT NULL,
+    role_id SMALLINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (role_id)
+        REFERENCES roles (id)
+);
+
+INSERT INTO roles (name)
+VALUES ('ROLE_USER');
+
+INSERT INTO users (password, login, email)
+VALUES ('$2y$12$G3.H5B2vrDOfmrwl/he/8.IZ12zNXHqmRbm2FVoMiDdqkOHZCf8vW', 'admin', 'admin@gmail.com');
+
+INSERT INTO users_roles (user_id, role_id)
+VALUES (1, 1);
+
+DROP TABLE IF EXISTS quotes;
+CREATE TABLE quotes
+(
+    id        SMALLSERIAL,
+    valute_id VARCHAR(7),
+    code      VARCHAR(3),
+    nominal   SMALLINT,
+    name      VARCHAR(255),
+    value     NUMERIC(6, 4),
+    date      DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_quotes
+(
+    user_id  SMALLINT NOT NULL,
+    quote_id SMALLINT NOT NULL,
+    PRIMARY KEY (user_id, quote_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (quote_id)
+        REFERENCES roles (id)
+);
